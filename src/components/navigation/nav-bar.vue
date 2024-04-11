a797a3816af3266a3c524e1144a27176db5ef1fb
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2024-04-11 14:39:50 +0200

    navbar final fix

--- src/components/navigation/nav-bar.vue
+++ src/components/navigation/nav-bar.vue
@@ -8,8 +8,8 @@
       <LogoutButton />
       <router-link
         to="/add-recipe"
-        class="before:ease relative h-16 w-40 overflow-hidden border border-green-500 bg-green-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40 flex justify-items-center items-center rounded text-center"
-        >Add Recipe</router-link
+        class="before:ease relative h-16 w-40 overflow-hidden border border-green-500 bg-green-500 text-white shadow-xl transition-all before:absolute before:right-0 before:top-0 before:h-16 before:w-6 before:translate-x-16 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40 text-center rounded flex items-center"
+        ><span class="text-center w-full">Add Recipe</span></router-link
       >
       <img alt="profilepic" :src="user.picture" class="rounded-full min-w-16 min-h-16 w-16 h-16" />
     </template>
