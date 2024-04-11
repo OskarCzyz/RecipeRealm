51102c7132fa88bf65281019930009aee17ab02e
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2024-04-11 09:35:06 +0200

    Ingredients checkbox

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -61,10 +61,26 @@ export default {
       alt="food image"
     />
     <p>Servings: {{ recipies.Servings }}</p>
-    <p class="m-5 p-5 border">
+    <div class="m-5 p-5 border">
       Ingriedients: <br />
-      {{ recipies.Ingriedients }}
-    </p>
+      <ul v-for="ingriedient in recipies.Ingriedients?.split('\n')" :key="ingriedient">
+        <li
+          v-if="ingriedient.trim() != ''"
+          class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+        >
+          <div class="flex items-center ps-3">
+            <input
+              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              type="checkbox"
+              :name="ingriedient"
+            /><label class="w-full py-3 ms-2 text-sm font-medium" :for="ingriedient">{{
+              ingriedient
+            }}</label>
+          </div>
+        </li>
+      </ul>
+      {{}}
+    </div>
     <hr />
     <p class="m-5 p-5 border">
       Instructions: <br />
